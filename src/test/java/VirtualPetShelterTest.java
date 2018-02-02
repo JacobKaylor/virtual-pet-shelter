5efb7ca738b82bc28f734e025d49a34b562527b5
JacobKaylor <jkaylor20@yahoo.com>
2018-02-02 13:21:18 -0500

    Allowing adoption test

--- src/test/java/VirtualPetShelterTest.java
+++ src/test/java/VirtualPetShelterTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -45,10 +46,18 @@ public class VirtualPetShelterTest {
 	}
 	@Test
 	public void shouldAllowAdoption() {
+		VirtualPet anotherPet = new VirtualPet("jerry", "large");
 		underTest.subtract(pet);
+		underTest.add(anotherPet);
 		
-		VirtualPet retrieved = underTest.findPet("jim");
-		assertThat(retrieved, org.hamcrest.Matchers.is(pet));
+		Collection<VirtualPet> pets = underTest.pets();
+
+		assertThat(pets, org.hamcrest.Matchers.contains(anotherPet));
+
+		assertFalse(pets.contains(pet));
+		assertTrue(pets.contains(anotherPet));
+		assertEquals(1, pets.size());
+
 	}
 
 
